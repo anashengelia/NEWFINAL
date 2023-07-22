4e1a112c06f280699985733676c34595611ad439
anashengelia <135009181+anashengelia@users.noreply.github.com>
2023-07-22 21:39:54 +0400

    article.html
    about article

---
+++ index.js
@@ -0,0 +1,14 @@
+let burgerIcon = document.querySelector(".fa-bars");
+let xMark = document.querySelector(".fa-xmark");
+
+burgerIcon.addEventListener("click", ()=>{
+    mobileMenu.classList.add("show");
+    burgerIcon.style.display = "none";
+    xMark.style.display = "block";
+})
+
+xMark.addEventListener("click", ()=>{
+    mobileMenu.classList.remove("show");
+    burgerIcon.style.display = "block";
+    xMark.style.display = "none";
+})
